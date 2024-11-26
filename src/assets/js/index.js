de69fd82e48abe5ee6a759e1bdfb786e6b14c7dc
Kato1245 <126714286+Kato1245@users.noreply.github.com>
2024-11-26 16:18:40 -0500

    2

---
+++ src/assets/js/index.js
@@ -0,0 +1,4 @@
+/* Preloader-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+setTimeout(function () {
+    document.querySelector(".loader_bg").classList.add("hidden");
+}, 1500); // Oculta el loader después de 1,5 segundos
